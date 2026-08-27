afe04af8869c3c9b6a913ed78a31c113af3fcfc6
pravindnikam07 <pravindnikam07@gmail.com>
2026-08-27 20:40:57 +0530

    Inclass notes added

---
+++ Inclass app/FirstProgram/Program.cs
@@ -0,0 +1,80 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, Pravin!");
+
+using System;
+
+namespace MyApp
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // Student s1 = new Student();
+      // s1.Marks = 95;
+      // s1.Rollno = 101;
+      // Console.WriteLine("Marks: " + s1.Marks);
+      // Console.WriteLine("Roll no: " + s1.Rollno);
+
+      // Payment payment = new UPI();
+      // payment.Pay();
+
+      // Animal animal = new Dog();
+      // animal.Sound();
+    }
+
+  }
+}
+
+abstract class Payment
+{
+  public abstract void Pay();
+}
+
+class UPI : Payment
+{
+    public override void Pay()
+    {
+        Console.WriteLine("Payment through UPI");
+    }
+}
+
+class Student
+{
+  private int marks;
+  private int rollno;
+
+  public int Rollno
+  {
+    get
+    {
+      return rollno;
+    }
+    set
+    {
+      if (value < 0)
+      {
+        Console.WriteLine("Roll number cannot be negative.");
+      }
+      else
+        rollno = value;
+    }
+  }
+
+  public int Marks { get; set; }
+}
+
+class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("Animal Sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
---
+++ Inclass app/oopdemo/Program.cs
@@ -0,0 +1,102 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace oopdemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // student
+      // string name = "John";
+      // int roll = 2;
+      // double marks = 90.4;
+
+      // Console.WriteLine("Name: " + name);
+      // Console.WriteLine("Roll: " + roll);
+      // Console.WriteLine("Marks: " + marks);
+
+      // string name1 = "Joseph";
+      // int roll1 = 5;
+      // string mobile = "9876543210";
+      // double marks1 = 95.4;
+
+      // Console.WriteLine("Name: " + name1);
+      // Console.WriteLine("Roll: " + roll1);
+      // Console.WriteLine("Marks: " + marks1);
+
+
+      // Student s1 = new Student();
+      // s1.name = "Ravi";
+      // s1.roll = 2;
+      // s1.marks = 90.4;
+      // s1.printDetails();
+
+      // Student s1 = new Student("Abhay", 2, 90.8);
+      // s1.printDetails();
+
+      // Student s2 = new Student("Ravi", 3, 96.8);
+      // s2.printDetails();
+
+
+      Animal dog = new Dog();
+      dog.sound();
+      dog.eat();
+
+    }
+  }
+}
+
+public abstract class Animal
+{
+
+  // concrete method => Is having body/ with implemenation
+  public virtual void sound()
+  {
+    Console.WriteLine("Animal makes sound.");
+  }
+  // abstract method => is does not have body / without implementation
+  public abstract void eat();
+}
+
+public class Dog : Animal
+{
+
+  public override void sound()
+  {
+    Console.WriteLine("Dog barks.");
+  }
+
+  public override void eat()
+  {
+    Console.WriteLine("Dog eats meat.");
+  }
+}
+
+
+class Student
+{
+  public string name;
+  public int roll;
+  public double marks;
+
+  public Student(string name, int roll, double marks)
+  {
+    this.name = name;
+    this.roll = roll;
+    this.marks = marks;
+  }
+
+  public void printDetails()
+  {
+    Console.WriteLine("Name: " + name);
+    Console.WriteLine("Roll: " + roll);
+    Console.WriteLine("Marks: " + marks);
+  }
+}
+
+
+
+
